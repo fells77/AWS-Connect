db58a47e19b3dd725b7decf99ed2658761ba2d05
sfell77 <github@sfproductions.net>
2025-02-13 16:30:52 -0500

    forgot

---
+++ chatRequest.js
@@ -0,0 +1,10 @@
+import { S3Client, ListBucketsCommand } from "@aws-sdk/connect";
+var AWS = require("aws-sdk");
+
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log("Access key:", AWS.config.credentials.accessKeyId);
+  }
+});
